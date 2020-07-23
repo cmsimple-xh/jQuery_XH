cb375068ccd9e224fd17914c8a21b5a8efa9f6a9
TN03 <h.irmler@t-online.de>
2020-07-23 18:39:07 +0200

    Update language files
    Add a hint for migrate plugin

--- languages/default.php
+++ languages/default.php
@@ -1,8 +1,7 @@
 <?php
 
 	$plugin_tx['jquery']['cf_autoload']="Check to auto-load the libraries on every page.";
-        $plugin_tx['jquery']['cf_autoload_libraries']="Whether to auto-load only jQuery or jQuery together with jQueryUI.";
-        $plugin_tx['jquery']['cf_load_migrate']="Whether to load the Migrate-plugin or not.";
-	$plugin_tx['jquery']['utf-8-marker']="äöü";
+    $plugin_tx['jquery']['cf_autoload_libraries']="Whether to auto-load only jQuery or jQuery together with jQueryUI.";
+    $plugin_tx['jquery']['cf_load_migrate']="Whether to load the Migrate-plugin or not.";
 
 ?>
