555be5c44ec2ab1600219b5a3f68242a52ed596b
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2023-06-22 07:51:55 +0200

    Change the basic setting

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 $plugin_cf['jquery']['version_core']="3.7.0";
 $plugin_cf['jquery']['version_ui']="1.13.2";
-$plugin_cf['jquery']['version_migrate']="jquery-migrate-3.4.0.min.js";
-$plugin_cf['jquery']['load_migrate']="true";
+$plugin_cf['jquery']['version_migrate']="jquery-migrate-3.4.1.min.js";
+$plugin_cf['jquery']['load_migrate']="";
 $plugin_cf['jquery']['autoload']="";
 $plugin_cf['jquery']['autoload_libraries']="jQuery";
